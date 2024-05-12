c9d16cdb7e194e6e5c02c01878c82e573f99d4bc
Whiteastaroth <125083144+Whiteastaroth@users.noreply.github.com>
2024-05-12 20:58:10 +0300

    Add files via upload

---
+++ E3.js
@@ -0,0 +1,12 @@
+const ArrowFunction =( x, n) => {
+    let result = x
+
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+const result = ArrowFunction(4, 3)
+console.log(result)
