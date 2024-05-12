70577e2d59f080e85430ababc326316a69175e4e
Whiteastaroth <125083144+Whiteastaroth@users.noreply.github.com>
2024-05-12 21:51:42 +0300

    Add files via upload

--- E3.js
+++ E3.js
@@ -1,5 +1,5 @@
-const Arrow =( x, n) => {
-    console.log ( x ** n);   //Используется функция ArrowFunction
+const Arrow =( x, n) => {     //Используется функция ArrowFunction
+    console.log ( x ** n);      // возвращает x в степени n
 }
 
 Arrow (6, 3);   // первая цифра "n" вторая цифра "n"
